5b17dec47e295b471515433f824198caa5e190a4
Aravind Balla <bsaaravind@gmail.com>
2015-12-07 17:24:55 +0530

    Submit Commit

--- src/012sllSort.cpp
+++ src/012sllSort.cpp
@@ -22,5 +22,36 @@ struct node {
 };
 
 void sll_012_sort(struct node *head){
+
+	int k = 0, i, t;
+	int count[3] = { 0 }; //contains the count of 0, 1 and 2 respectively.
+	struct node *p;
+
+	p = head;
+
+	while (p != NULL)
+	{
+		count[p->data]++;
+		p = p->next;
+	}
+
+	p = head;
+
+	for (i = 0; i < count[0]; i++)
+	{
+		p->data = 0;
+		p = p->next;
+	}
+	for (i = 0; i < count[1]; i++)
+	{
+		p->data = 1;
+		p = p->next;
+	}
+	for (i = 0; i < count[2]; i++)
+	{
+		p->data = 2;
+		p = p->next;
+	}
+	
 	
 }
